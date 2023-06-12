df8fdd9e2be68dc871d43bb814b87450abc2f520
2424005179@qq.com <2424005179@qq.com>
2023-06-13 01:32:29 +0800

    dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   // js执行文件入口
   entry: './main.js',
   output: {
     // 将所有依赖的模块合并输出到bundle.js文件
     filename: 'bundle.js',
-
     // 将输出文件都放到dist文件夹
     path: path.resolve(__dirname, './dist')
 
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `[name]_[contenthash:8].css`
+    })
+  ]
 }
